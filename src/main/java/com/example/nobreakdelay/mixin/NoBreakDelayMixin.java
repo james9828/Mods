d8e61c1a0c1011bf88652baad66ab8349ddb5cfd
Your Name <youremail@example.com>
2025-12-28 10:06:27 +0800

    build

--- src/main/java/com/example/nobreakdelay/mixin/NoBreakDelayMixin.java
+++ src/main/java/com/example/nobreakdelay/mixin/NoBreakDelayMixin.java
@@ -1,19 +1,19 @@
 package com.example.nobreakdelay.mixin;
 
-import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayerInteractionManager.class)
+@Mixin(MultiPlayerGameMode.class)
 public class NoBreakDelayMixin {
 
-    @Shadow private int blockBreakingCooldown;
+    @Shadow private int destroyDelay;
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void removeDelay(CallbackInfo ci) {
-        this.blockBreakingCooldown = 0;
+        this.destroyDelay = 0;
     }
 }
